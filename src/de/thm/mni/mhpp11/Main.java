071e73bb784475acc734abce94c140c8f7c53b69
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2016-09-10 17:33:35 +0200

    add MainApp for real App and change Main to start MainApp

--- src/de/thm/mni/mhpp11/Main.java
+++ src/de/thm/mni/mhpp11/Main.java
@@ -1,5 +1,9 @@
 package de.thm.mni.mhpp11;
 
+import de.thm.mni.mhpp11.parser.ASTParser;
+import de.thm.mni.mhpp11.parser.ParserException;
+import de.thm.mni.mhpp11.util.UTF8ResourceBundleControl;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -9,13 +13,36 @@ import java.util.ResourceBundle;
  */
 public class Main {
   
-  public static void main( String[] args ) {
+  public static void main(String[] args) {
+    Main m = new Main();
+    m.startGUI(args);
+    //m.internationalHelloWorld(args);
+  }
+  
+  private void startGUI(String[] args) {
+    MainApp.launch(MainApp.class, args);
+  }
+  
+  private void internationalHelloWorld(String[] args) {
     String language = (args.length != 2) ? "en" : args[0];
-    String country = (args.length != 2) ? "US" : args[1];;
-
+    String country = (args.length != 2) ? "US" : args[1];
+    
     Locale currentLocale = new Locale(language, country);
-    ResourceBundle messages = ResourceBundle.getBundle("de/thm/mni/mhpp11/i18n/Default", currentLocale);
-
+    System.out.println(new UTF8ResourceBundleControl().getFallbackLocale("de/thm/mni/mhpp11/i18n/Default", currentLocale));
+    ResourceBundle messages = ResourceBundle.getBundle("de/thm/mni/mhpp11/i18n/Default", currentLocale, new UTF8ResourceBundleControl());
     System.out.println(messages.getString("hello_world"));
   }
+  
+  private void testJModelica() {
+    String path = "./test.mo";
+    path = "/home/hobbypunk/Dokumente/Entwicklung/THM_Projekte/Projektphase/2014-modelica-kotani/SHM/SeidelThesis/Examples/FullModel/package.mo";
+  
+    ASTParser ap = ASTParser.getInstance();
+    try {
+      ap.parse(path);
+    } catch(ParserException e) {
+      e.printStackTrace();
+    }
+  }
+  
 }
---
+++ src/de/thm/mni/mhpp11/MainApp.java
@@ -0,0 +1,48 @@
+package de.thm.mni.mhpp11;
+
+import de.thm.mni.mhpp11.util.Settings;
+import de.thm.mni.mhpp11.util.UTF8ResourceBundleControl;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+import java.util.ResourceBundle;
+
+
+/**
+ * Created by hobbypunk on 10.09.16.
+ */
+public class MainApp extends Application {
+  
+  private ResourceBundle i18n;
+  private Settings settings;
+  
+  public MainApp() {
+    try {
+      settings = Settings.load();
+    } catch(ConfigurationException e) {
+      e.printStackTrace();
+    }
+    i18n = ResourceBundle.getBundle("de/thm/mni/mhpp11/i18n/welcome", settings.getLang(), new UTF8ResourceBundleControl());
+  }
+  
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setTitle(i18n.getString("title.title"));
+    
+    FXMLLoader loader = new FXMLLoader();
+    loader.setResources(i18n);
+    loader.setLocation(Main.class.getResource("view/welcome.fxml"));
+    HBox rootLayout = loader.load();
+    
+    // Show the scene containing the root layout.
+    Scene scene = new Scene(rootLayout);
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+    primaryStage.centerOnScreen();
+    primaryStage.show();
+  }
+}
