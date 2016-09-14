bcbab8163f28cff831a3076f5a81d0526596ac86
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2016-09-14 19:21:32 +0200

    add in window popup notifications

---
+++ src/de/thm/mni/mhpp11/control/NotificationControl.java
@@ -0,0 +1,60 @@
+package de.thm.mni.mhpp11.control;
+
+import de.thm.mni.mhpp11.util.config.Settings;
+import de.thm.mni.mhpp11.util.logger.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+/**
+ * Created by hobbypunk on 13.09.16.
+ */
+public class NotificationControl extends VBox implements Initializable {
+  
+  @FXML private Label lHeadline;
+  @FXML private Label lMessage;
+  @FXML private FontIcon fIcon;
+  
+  private final Logger.Message message;
+  
+  public NotificationControl(Logger.Message message) {
+    this.message = message;
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("view/Notification.fxml"));
+    loader.setRoot(this);
+    loader.setController(this);
+    try {
+      loader.load();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    try {
+      Settings s = Settings.load();
+      lHeadline.textProperty().set(message.getHeadline());
+      lMessage.textProperty().set(message.getMessage());
+      Logger.Message.TYPE type = message.getType();
+      String icon = ":22:";
+      if(type.isInfo()) {
+        icon = "gmi-info" + icon + "cornflowerblue";
+      } else if(type.isWarning()) {
+        icon = "gmi-warning" + icon + "darkorange";
+      } else if(type.isError()) {
+        icon = "gmi-error" + icon + "firebrick";
+      } else
+        icon = "gmi-settings-input-component" + icon + "darkgrey";
+      fIcon.setIconLiteral(icon);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
--- src/de/thm/mni/mhpp11/controller/NotifyController.java
+++ src/de/thm/mni/mhpp11/controller/NotifyController.java
@@ -1,7 +1,64 @@
 package de.thm.mni.mhpp11.controller;
 
+import de.thm.mni.mhpp11.control.NotificationControl;
+import de.thm.mni.mhpp11.util.logger.Logger.Message;
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.ResourceBundle;
+
 /**
  * Created by hobbypunk on 13.09.16.
  */
-public class NotifyController {
+public abstract class NotifyController extends Controller implements Observer {
+  
+  @FXML protected GridPane root;
+  @FXML private VBox notifications;
+  
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    super.initialize(location, resources);
+    logger.addObserver(this);
+    settings.addObserver(this);
+  }
+  
+  @Override
+  public void deinitialize() {
+    super.deinitialize();
+    logger.deleteObserver(this);
+  }
+  
+  public void addNotification(final NotificationControl notification) {
+    notifications.getChildren().add(notification);
+    notification.setOpacity(0.0);
+    FadeTransition fadeIn = new FadeTransition(Duration.millis(250), notification);
+    fadeIn.setFromValue(0.0);
+    fadeIn.setToValue(1.0);
+    fadeIn.setOnFinished(e1 -> {
+      PauseTransition delay = new PauseTransition(Duration.seconds(settings.getNotifySeconds()));
+      delay.setOnFinished(e2 -> {
+        FadeTransition fadeOut = new FadeTransition(Duration.millis(250), notification);
+        fadeOut.setFromValue(notification.getOpacity());
+        fadeOut.setToValue(0.0);
+        fadeOut.setOnFinished(e3 -> notifications.getChildren().remove(notification));
+        fadeOut.play();
+      });
+      delay.play();
+    });
+    fadeIn.play();
+  }
+  
+  public void update(Observable o, Object arg) {
+    if(arg instanceof Message) {
+      Message msg = (Message) arg;
+      this.addNotification(new NotificationControl(msg));
+    }
+  }
 }
