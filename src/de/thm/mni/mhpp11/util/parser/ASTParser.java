1355fee83a3f5e937c678e993d0489c771b18858
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2016-09-14 19:18:10 +0200

    read within and package from Modelica files

--- src/de/thm/mni/mhpp11/util/parser/ASTParser.java
+++ src/de/thm/mni/mhpp11/util/parser/ASTParser.java
@@ -1,6 +1,7 @@
 package de.thm.mni.mhpp11.util.parser;
 
 import beaver.Parser.Exception;
+import de.thm.mni.mhpp11.model.MoPackage;
 import de.thm.mni.mhpp11.model.MoProgramm;
 import de.thm.mni.mhpp11.model.MoWithin;
 import org.jmodelica.modelica.compiler.*;
@@ -8,7 +9,9 @@ import org.jmodelica.util.OptionRegistry;
 import org.jmodelica.util.OptionRegistry.LocalIteration;
 import org.jmodelica.util.streams.CodeStream;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 /**
@@ -24,13 +27,13 @@ public class ASTParser {
   
   private ASTParser() {}
   
-  public MoProgramm parse(String file) throws ParserException {
+  public MoProgramm parse(File file) throws ParserException {
     OptionRegistry or = ModelicaCompiler.createOptions();
     or.setStringOption("local_iteration_in_tearing", LocalIteration.ALL);
     SourceRoot sr;
     
     try {
-      sr = ParserHandler.parseModelicaFile(UtilInterface.create(or), file);
+      sr = ParserHandler.parseModelicaFile(UtilInterface.create(or), file.getAbsolutePath());
       return parse(sr);
       
     } catch(Exception | IOException e) {
@@ -40,22 +43,40 @@ public class ASTParser {
   }
   
   public MoProgramm parse(SourceRoot sr) {
-    Program p = (Program)sr.getChild(0);
-    print(p);
+    Program p = sr.getProgram();
     return parse(p);
   }
   
   private MoProgramm parse(Program p) {
-    MoProgramm mp = new MoProgramm("","");
+    MoProgramm mp = new MoProgramm("");
     StoredDefinition sd = (StoredDefinition)p.getChild(0).getChild(0);
     mp.setWithin(parseWithin(sd));
+    mp.setPackage(parsePackage(sd));
     return mp;
   }
   
   private MoWithin parseWithin(StoredDefinition sd) {
-    if(sd.getWithin() == null) return null;
-    Within w = sd.getWithin();
-    return new MoWithin(w.getPackageName().toString());
+    java.util.List<String> packageList = new ArrayList<>();
+    if(sd.getWithin() == null) return new MoWithin(packageList);
+    Access a =sd.getWithin().getPackageName();
+    if(a instanceof ParseAccess) {
+     packageList.add(((ParseAccess) a).getID());
+    } else {
+      for (int i = 0; i < a.getChild(0).getNumChild(); i++) {
+        ParseAccess pa = (ParseAccess) a.getChild(0).getChild(i);
+        packageList.add(pa.getID());
+      }
+    }
+    return new MoWithin(packageList);
+  }
+  
+  private MoPackage parsePackage(StoredDefinition sd) {
+    FullClassDecl fcd = (FullClassDecl)sd.getChild(1).getChild(0);
+    if(!(fcd.getChild(4) instanceof MPackage)) return null;
+    String name = fcd.getName().getID();
+    StringComment sc = fcd.getStringComment();
+    String comment = (sc == null)? null: sc.getComment();
+    return new MoPackage(name, comment);
   }
   
   private void print(List<? extends ClassDecl> list) {
---
+++ src/de/thm/mni/mhpp11/util/parser/PackageParser.java
@@ -0,0 +1,46 @@
+package de.thm.mni.mhpp11.util.parser;
+
+import de.thm.mni.mhpp11.model.MoProgramm;
+import de.thm.mni.mhpp11.util.config.Settings;
+import de.thm.mni.mhpp11.util.logger.Logger;
+
+import java.io.File;
+
+/**
+ * Created by hobbypunk on 14.09.16.
+ */
+public class PackageParser {
+  private static PackageParser ourInstance = new PackageParser();
+  
+  public static PackageParser getInstance() {
+    return ourInstance;
+  }
+  
+  private ASTParser parser;
+  private Logger logger;
+  
+  private PackageParser() {
+    Settings settings = Settings.load();
+    logger = settings.getLogger();
+    parser = ASTParser.getInstance();
+  }
+  
+  public File findBasePackage(File f) {
+    try {
+      MoProgramm mp = parser.parse(f);
+      if(mp.getPackage() != null || mp.getWithin().getPackages().size() > 0) {
+        f = f.getParentFile();
+        if(mp.getPackage() != null && mp.getWithin().getPackages().size() > 0) {
+          f = f.getParentFile();
+        }
+        for(Integer i = 1; i < mp.getWithin().getPackages().size(); i++) {
+          f = f.getParentFile();
+        }
+        return new File(f.getAbsolutePath() + "/package.mo");
+      }
+    } catch(ParserException e) {
+      logger.error(e);
+    }
+    return f;
+  }
+}
