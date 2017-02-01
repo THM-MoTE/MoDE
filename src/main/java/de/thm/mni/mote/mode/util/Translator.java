f8e7a6bdcd22e02e1defb35ce2fea82ddf0d9d68
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2017-02-01 18:13:46 +0100

    bugfix: remove @UtilityClass

--- src/main/java/de/thm/mni/mote/mode/util/Translator.java
+++ src/main/java/de/thm/mni/mote/mode/util/Translator.java
@@ -1,29 +1,26 @@
 package de.thm.mni.mote.mode.util;
 
-import lombok.experimental.UtilityClass;
-
 import java.text.MessageFormat;
 import java.util.*;
 
 /**
  * Created by hobbypunk on 30.01.17.
  */
-@UtilityClass
-public class Translator {
-  private Map<String, ResourceBundle> bundles = new HashMap<>();
+public final class Translator {
+  private static Map<String, ResourceBundle> bundles = new HashMap<>();
   
-  private ResourceBundle getBundle(String bundle) {
+  public static ResourceBundle getBundle(String bundle) {
     if (!bundles.containsKey(bundle)) {
       bundles.put(bundle, Utilities.getBundle(bundle));
     }
     return bundles.get(bundle);
   }
   
-  public String tr(String bundle, String key) {
+  public static String tr(String bundle, String key) {
     return tr(getBundle(bundle), key);
   }
   
-  public String tr(ResourceBundle bundle, String key) {
+  public static String tr(ResourceBundle bundle, String key) {
     try {
       return bundle.getString(key);
     } catch (MissingResourceException e) {
@@ -32,12 +29,12 @@ public class Translator {
   }
   
   
-  public String tr(String bundle, String key, Object... params) {
+  public static String tr(String bundle, String key, Object... params) {
     return tr(getBundle(bundle), key, params);
   }
   
   
-  public String tr(ResourceBundle bundle, String key, Object... params) {
+  public static String tr(ResourceBundle bundle, String key, Object... params) {
     try {
       return MessageFormat.format(bundle.getString(key), params);
     } catch (MissingResourceException e) {
@@ -45,11 +42,11 @@ public class Translator {
     }
   }
   
-  public String tr(String bundle, String key, Integer splitAt, Object... params) {
+  public static String tr(String bundle, String key, Integer splitAt, Object... params) {
     return tr(getBundle(bundle), key, splitAt, params);
   }
   
-  public String tr(ResourceBundle bundle, String key, Integer splitAt, Object... params) {
+  public static String tr(ResourceBundle bundle, String key, Integer splitAt, Object... params) {
     try {
       Object[] p1 = Arrays.copyOfRange(params, 0, splitAt);
       Object[] p2 = Arrays.copyOfRange(params, splitAt, params.length);
