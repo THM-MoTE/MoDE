c676e4bf70e6dd992175e7ca171ee2adcb1277d0
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2017-12-11 16:07:13 +0100

    fix possible wrong language on first start

--- src/main/java/de/thm/mni/mote/mode/frontend/actors/NotifyActor.java
+++ src/main/java/de/thm/mni/mote/mode/frontend/actors/NotifyActor.java
@@ -3,17 +3,24 @@ package de.thm.mni.mote.mode.frontend.actors;
 import de.thm.mni.mhpp11.smbj.logging.messages.LogMessage;
 import de.thm.mni.mhpp11.smbj.messages.base.Message;
 import de.thm.mni.mhpp11.smbj.ui.actors.AbstractUIActor;
+import de.thm.mni.mote.mode.config.Settings;
 import de.thm.mni.mote.mode.frontend.controllers.NotifyController;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 abstract class NotifyActor<C extends NotifyController> extends AbstractUIActor<C> {
   
   NotifyActor(UUID id, Stage stage, List<?> params) {
     super(id, stage, params);
+  }
+  
+  @Override
+  protected Locale getLocale() {
+    return Settings.load().getLang();
   }
   
   @Override
